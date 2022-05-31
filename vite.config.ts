20908b5f21017acd95b327b214296267934c7327
Lukas <lukas.fischer1@outlook.com>
2022-05-31 11:29:58 +0200

    removed old config

--- vite.config.ts
+++ vite.config.ts
@@ -6,22 +6,6 @@ import removeConsole from "vite-plugin-remove-console"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react(), removeConsole()],
-    server: {
-        proxy: {
-            "/api": {
-                target: "http://192.168.0.10:3001",
-                changeOrigin: true,
-                secure: false,
-                ws: true
-            },
-            "/socket.io": {
-                target: "http://192.168.0.10:3001",
-                changeOrigin: true,
-                secure: false,
-                ws: true
-            }
-        }
-    },
     build: {
         rollupOptions: {
             plugins: [visualizer()]
