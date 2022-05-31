c754f0f49a6255870dd0fc474348afed536a55f9
Lukas <lukas.fischer1@outlook.com>
2022-05-31 11:29:02 +0200

    vite-plugin-remove-console added

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite"
-import visualizer from "rollup-plugin-visualizer"
 import react from "@vitejs/plugin-react"
+import visualizer from "rollup-plugin-visualizer"
+import { defineConfig } from "vite"
+import removeConsole from "vite-plugin-remove-console"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), removeConsole()],
     server: {
         proxy: {
             "/api": {
