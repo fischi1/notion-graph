519da6a746ae784c87f23d67b43918023a60e2d3
Lukas <lukas.fischer1@outlook.com>
2022-07-25 22:23:07 +0200

    dropping files onto the panel

--- src/ui/views/SelectFile.tsx
+++ src/ui/views/SelectFile.tsx
@@ -1,9 +1,10 @@
+import { useDropzone } from "react-dropzone"
+import SelectFileDialogContent from "../components/dialogContent/SelectFileDialogContent"
 import Dialog from "../components/presentation/Dialog"
 import FileInputButton from "../components/presentation/FileInputButton"
 import Heading from "../components/presentation/Heading"
 import Link from "../components/presentation/Link"
 import Panel from "../components/presentation/Panel"
-import SelectFileDialogContent from "../components/dialogContent/SelectFileDialogContent"
 import { useStoreDispatch } from "../components/Store"
 import useHashNav from "../hooks/useHashNav"
 
@@ -15,31 +16,80 @@ const SelectFile = () => {
         dispatch({ type: "fileChosen", file })
     }
 
+    const accept: Record<string, string[]> = {}
+
+    accept["application/zip"] = [".zip"]
+
+    if (!import.meta.env.PROD) {
+        accept["application/json"] = [".json"]
+    }
+
+    const { getRootProps, isDragActive } = useDropzone({
+        accept: accept,
+        noClick: true,
+        noKeyboard: true,
+        multiple: false,
+        onDropAccepted(files) {
+            console.log("dropped", files)
+            if (files.length === 1) {
+                handleFileChosen(files[0])
+            }
+        }
+    })
+
     return (
         <Panel>
-            <Heading>1. Choose or drop Notion-Export ZIP</Heading>
-            <Link href="#zip-help-modal">Where can I get this file from?</Link>
-            <br />
-            <br />
-            <div>
-                <FileInputButton
-                    accept={
-                        import.meta.env.PROD
-                            ? "application/zip"
-                            : "application/zip,application/json"
-                    }
-                    onSelected={handleFileChosen}
-                >
-                    Choose file
-                </FileInputButton>
-            </div>
-            <Dialog
-                open={dialogOpen}
-                onClose={back}
-                title="Exporting Notion content"
+            <div
+                {...getRootProps()}
+                role={undefined}
+                style={{
+                    position: "relative"
+                }}
             >
-                <SelectFileDialogContent />
-            </Dialog>
+                {isDragActive && (
+                    <div
+                        style={{
+                            position: "absolute",
+                            width: "100%",
+                            height: "100%",
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center"
+                        }}
+                    >
+                        Drop here
+                    </div>
+                )}
+                <div
+                    style={{ visibility: isDragActive ? "hidden" : undefined }}
+                >
+                    <Heading>1. Choose or drop Notion-Export ZIP</Heading>
+                    <Link href="#zip-help-modal">
+                        Where can I get this file from?
+                    </Link>
+                    <br />
+                    <br />
+                    <div>
+                        <FileInputButton
+                            accept={
+                                import.meta.env.PROD
+                                    ? "application/zip"
+                                    : "application/zip,application/json"
+                            }
+                            onSelected={handleFileChosen}
+                        >
+                            Choose file
+                        </FileInputButton>
+                    </div>
+                </div>
+                <Dialog
+                    open={dialogOpen}
+                    onClose={back}
+                    title="Exporting Notion content"
+                >
+                    <SelectFileDialogContent />
+                </Dialog>
+            </div>
         </Panel>
     )
 }
